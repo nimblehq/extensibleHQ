7b0c800b9ded7c9af4721b77d90496d9b83def37
micky <tinn@nimbl3.com>
2018-10-12 09:59:34 +0700

    Add comment display toggle

--- src/commentToggle/index.js
+++ src/commentToggle/index.js
@@ -1,6 +1,18 @@
 import { observe } from  'selector-observer'
 
 export default () => {
+  const toggleCommentDisplay = (fileDiffContainer) => {
+    const commentContainers = fileDiffContainer.querySelectorAll('.comment-thread-container');
+
+    commentContainers.forEach((container) => {
+      if (container.style.display !== 'none') {
+        container.style.display = 'none'; 
+      } else {
+        container.style.display = 'block'; 
+      }
+    })
+  }
+
   observe('.diff-action, .secondary', {
     add(element) {
       const commentToggleButton = document.createElement('button');
@@ -8,6 +20,8 @@ export default () => {
       commentToggleButton.appendChild(commentToggleText);
       commentToggleButton.className = ('execute click aui-button aui-button-light sbs');
       element.prepend(commentToggleButton);
+      const fileDiffContainer = element.closest('.diff-container');
+      element.addEventListener('click', () => toggleCommentDisplay(fileDiffContainer));
     }
   })
 }
