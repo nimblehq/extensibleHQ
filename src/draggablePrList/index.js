85bf99c3aaa19674e7233efdfc0c79fb8288bdf6
Fikri Karim <fkfikrikarim@gmail.com>
2018-10-12 15:24:51 +0700

    Can sort PR

--- src/draggablePrList/index.js
+++ src/draggablePrList/index.js
@@ -4,12 +4,49 @@ import { Sortable } from '@shopify/draggable';
 export default () => {
   observe('tbody', {
     add(table) {
-      console.log(table);
+      console.log('active');
+      const order = ["141", "149", "137"]
+
+      let items = [].slice.call(table.children);
+      while (table.firstChild) {
+        table.removeChild(table.firstChild);
+      }
+
+      order.forEach((id) => {
+        const node = items.find((item) => {
+          return item.attributes['data-pull-request-id'].value === id
+        })
+        table.append(node);
+      });
+
+      let notInDatabase = items.filter((item) => {
+        return !order.includes(item.attributes['data-pull-request-id'].value)
+      })
+
+      console.log(notInDatabase);
+
+      notInDatabase.sort(function(a,b) { 
+        return a.attributes['data-pull-request-id'].value - b.attributes['data-pull-request-id'].value 
+      });
+      
+      notInDatabase.forEach(function(item) {
+        table.append(item);
+      })
+
       const sortable = new Sortable(table, {
         draggable: '.pull-request-row'
       })
 
-      console.log(sortable);
+      sortable.on('sortable:start', (evt) => {
+        console.log('sortable:start');
+        console.log(evt);
+      });
+
+      sortable.on('sortable:stop', (evt) => {
+        console.log('sortable:stop');
+        console.log(evt);
+      });
+    
     },
   })
 }
