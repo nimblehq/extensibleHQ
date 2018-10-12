d0adda9c0b1669cd2a94ed6b3b72989845eb180c
micky <tinn@nimbl3.com>
2018-10-12 10:47:50 +0700

    Sass setup

--- config/webpack.js
+++ config/webpack.js
@@ -1,7 +1,9 @@
 const path = require('path');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
+  entry: [path.resolve(__dirname, '../src/public/stylesheets/index.scss')],
   output: {
     path: path.resolve(__dirname, '../src/build'),
     filename: 'index.js'
@@ -23,13 +25,37 @@ module.exports = {
             options: { minimize: true }
           }
         ]
-      }
+      },
+      {
+				test: /\.scss$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: 'index.css',
+						}
+					},
+					{
+						loader: 'extract-loader'
+					},
+					{
+						loader: 'css-loader?-url'
+					},
+					{
+						loader: 'sass-loader'
+					}
+				]
+			}
     ]
   },
   plugins: [
     new HtmlWebPackPlugin({
       template: "./src/index.html",
       filename: "./index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
     })
   ],
   devtool: 'source-map'
