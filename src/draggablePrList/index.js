55635cfafd9ccd39dcf3eebe7f9180d4d2a0c381
Fikri Karim <fkfikrikarim@gmail.com>
2018-12-20 15:53:58 +0700

    Refactor draggablePrList

--- src/draggablePrList/index.js
+++ src/draggablePrList/index.js
@@ -1,57 +1,65 @@
 import { observe } from 'selector-observer'
 import { Sortable } from '@shopify/draggable';
 
+const getDatabaseOrder = () => {
+  const order = localStorage.getItem('prOrder');
+
+  return order === null ? [] : order.split(",");
+}
+
+const setDatabaseOrder = (order) => {
+  localStorage.setItem('prOrder', order);
+}
+
+const shiftElementArray = (array, from, to) => {
+  array.splice(to, 0, array.splice(from, 1)[0]);
+};
+
+const addMissingElementToArray = (arrayBig, arraySmall) => {
+  const missingElements = arrayBig.filter((element) => {
+    return !arraySmall.includes(element)
+  })
+
+  return [...arraySmall, ...missingElements]
+}
+
+const reorderPrList = (table, prListElements, order) => {
+  while (table.firstChild) {
+    table.removeChild(table.firstChild);
+  }
+
+  order.forEach((id) => {
+    const node = prListElements.find((prElement) => {
+      return prElement.attributes['data-pull-request-id'].value === id
+    })
+    table.append(node);
+  });
+}
+
+const initializeSortable = (table, order) => {
+  const sortable = new Sortable(table, {
+    draggable: '.pull-request-row'
+  })
+
+  sortable.on('sortable:stop', ({ oldIndex, newIndex }) => {
+    shiftElementArray(order, oldIndex, newIndex)
+    setDatabaseOrder(order);
+  });
+}
+
 export default () => {
   observe('tbody', {
     add(table) {
-      console.log('active');
-      const databaseOrder = localStorage.getItem('prOrder').split(",");
-
-      let items = [].slice.call(table.children);
-
-      while (table.firstChild) {
-        table.removeChild(table.firstChild);
-      }
-
-      databaseOrder.forEach((id) => {
-        const node = items.find((item) => {
-          return item.attributes['data-pull-request-id'].value === id
-        })
-        table.append(node);
+      const databaseOrder = getDatabaseOrder();
+      const prListElements = [].slice.call(table.children);
+      const bitBucketOrder = prListElements.map(prElement => {
+        return prElement.attributes['data-pull-request-id'].value
       });
 
-      let notInDatabase = items.filter((item) => {
-        return !databaseOrder.includes(item.attributes['data-pull-request-id'].value)
-      })
+      const order = addMissingElementToArray(bitBucketOrder, databaseOrder);
 
-      notInDatabase.sort(function(a,b) { 
-        return a.attributes['data-pull-request-id'].value - b.attributes['data-pull-request-id'].value 
-      });
-
-      let order = [].slice.call(table.children).map((item) => {
-        return item.attributes['data-pull-request-id'].value
-      });
-      console.log(order);
-      
-      notInDatabase.forEach(function(item) {
-        table.append(item);
-      })
-
-
-      const sortable = new Sortable(table, {
-        draggable: '.pull-request-row'
-      })
-
-      const move = function (array, from, to) {
-        array.splice(to, 0, array.splice(from, 1)[0]);
-      };
-
-      sortable.on('sortable:stop', ({oldIndex, newIndex}) => {
-        move(order, oldIndex, newIndex)
-        console.log(order);
-        localStorage.setItem('prOrder', order)
-      });
-    
+      reorderPrList(table, prListElements, order);
+      initializeSortable(table, order);
     },
   })
 }
